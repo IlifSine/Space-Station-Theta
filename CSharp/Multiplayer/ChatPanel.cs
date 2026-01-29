60e5410444051a6d3c62e2fc7cfb15f1bc9fdf9b
IlifSine <EgormrcotPC@yandex.ru>
2026-01-29 13:19:23 +0300

    Chat doesen't work

--- CSharp/Multiplayer/ChatPanel.cs
+++ CSharp/Multiplayer/ChatPanel.cs
@@ -7,14 +7,25 @@ public partial class ChatPanel : Panel
 	private string ChatServerPath = "/root/ChatServer";
 	private ChatServer chatServer;
 
-	public void _Ready()
+	public override void _Ready()
 	{
 		chatServer = GetNode<ChatServer>(ChatServerPath);
-		chatServer.
+		chatServer.AddClient(this);
 	}
 
 	private void MessageEnter()
 	{
-		chatServer.SendMessage(lineEdit.Text);
+		chatServer.ReceiveSentMessage(lineEdit.Text);
+		lineEdit.Clear();
+	}
+
+	public void MessageReceive(string Message)
+	{
+		Rpc("MessageReciveRpc");
+	}
+
+	private void MessageReceiveRpc(string Message)
+	{
+		richTextLabel.Text = richTextLabel.Text + "/n" + Message;
 	}
 }
--- CSharp/Multiplayer/ChatServer.cs
+++ CSharp/Multiplayer/ChatServer.cs
@@ -1,16 +1,34 @@
+using System.Collections.Generic;
 using Godot;
 
 public partial class ChatServer : Node
 {
-	public ChatPanel[] ChatClients = {};
+	public List<ChatPanel> ChatClients = new List<ChatPanel>();
 
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
 	public void AddClient(ChatPanel chatPanel)
 	{
-		ChatClients.Add()
+		if (Multiplayer.IsServer())
+		{
+			ChatClients.Add(chatPanel);
+		}
+		else
+		{
+			RpcId(1, "AddClient", chatPanel);
+		}
 	}
 
-	public void SendMessage()
+	public void ReceiveSentMessage(string Message)
 	{
-		
+		RpcId(1, "SendMessageServer", Message);
+	}
+
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+	private void SendMessage(string Message)
+	{
+		foreach (ChatPanel item in ChatClients)
+		{
+			item.MessageReceive(Message);
+		}
 	}
 }
